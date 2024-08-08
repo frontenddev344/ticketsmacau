b738dd539146b5ef7ebbb92769263ce1f32675f9
leetbadd288 <frontenddev344@gmail.com>
2024-08-08 18:28:28 +0530

    update

---
+++ javascript/custom.js
@@ -0,0 +1,97 @@
+$(document).ready(function () {
+    $(".open").click(function () {
+        $("body").addClass("toggle");
+    });
+});
+$(document).ready(function () {
+    $(".closebutton").click(function () {
+        $("body").removeClass("toggle");
+    });
+});
+
+AOS.init({
+    duration: 1200,
+})
+$(document).ready(function () {
+    $(".toggle_btn").click(function () {
+        var $this = $(this);
+        var elem = $this.text();
+        if (elem == "Katso lisää") {
+            //Stuff to do when btn is in the read more state
+            $this.text("Katso vähemmän");
+            $this.next(".content").slideDown();
+        } else {
+            //Stuff to do when btn is in the read less state
+            $this.text("Katso lisää");
+            $this.next(".content").slideUp();
+        }
+    });
+});
+$(document).ready(function () {
+    $(window).scroll(function () {
+        var header = $("header");
+        header.toggleClass("sticky", $(window).scrollTop() > 0);
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const searchIcon = document.getElementById("search-icon");
+    const popup = document.getElementById("search-popup");
+    const close = document.querySelector(".close");
+
+    searchIcon.addEventListener("click", function (event) {
+        event.preventDefault();
+        popup.style.display = "block";
+    });
+
+    close.addEventListener("click", function () {
+        popup.style.display = "none";
+    });
+
+    window.addEventListener("click", function (event) {
+        if (event.target == popup) {
+            popup.style.display = "none";
+        }
+    });
+});
+(function () {
+    "use strict";
+
+    var cookieAlert = document.querySelector(".cookie-alert");
+    var acceptCookies = document.querySelector(".accept-cookies");
+
+    cookieAlert.offsetHeight; // Force browser to trigger reflow (https://stackoverflow.com/a/39451131)
+
+    if (!getCookie("acceptCookies")) {
+        cookieAlert.classList.add("show");
+    }
+
+    acceptCookies.addEventListener("click", function () {
+        setCookie("acceptCookies", true, 60);
+        cookieAlert.classList.remove("show");
+    });
+})();
+
+// Cookie functions 
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) === ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) === 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
